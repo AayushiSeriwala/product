efdaec7fa8102dcd75502c3870484a32501a4810
AayushiSeriwala <09aayushi19@gmail.com>
2022-03-30 12:31:00 +0530

    hkkkl

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
+const cors=require('cors');
 const IndexRouter=require('./routes/index.routes');
 mongoose.connect('mongodb+srv://aman:12365@cluster0.vigvo.mongodb.net/Users');
 
 const app=express();
+app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
